6454dabb2e3f93c143fcff4c546d46fb086e5808
Oct - test <oct@email.com>
2025-05-14 00:13:20 +0530

    epi3 done still handling the error gracefully

--- howToCreateThePromise/src/script.js
+++ howToCreateThePromise/src/script.js
@@ -1,5 +1,5 @@
-const cart = ["product1", "product2", "product3"];
-// const cart = [];
+// const cart = ["product1", "product2", "product3"];
+const cart = [];
 
 // consuming the pormise
 const user = createOrder(cart);
@@ -19,7 +19,9 @@ function createOrder(cart) {
         // after we'll be going the orderId from the backend
         const orderId = "orderId";
         if (orderId) {
-            resolve(orderId);
+            setInterval(function () {
+                resolve(orderId);
+            }, 5000);
         }
     });
 
@@ -32,4 +34,8 @@ function validOrder(cart) {
     return cart.length > 0;
 }
 
-user.then((data) => console.log(data));
+user
+    .then((data) => { console.log(data) })
+    .catch(function (err) {
+        console.log(err.message)
+    })
