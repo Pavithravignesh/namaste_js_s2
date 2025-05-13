a7ad66b0be0c4aff24a5dff98a116c97d2ecc581
Oct - test <oct@email.com>
2025-05-14 00:05:22 +0530

    epi 3 i knewn how to create a promise and the stuffs it having

---
+++ howToCreateThePromise/src/script.js
@@ -0,0 +1,35 @@
+const cart = ["product1", "product2", "product3"];
+// const cart = [];
+
+// consuming the pormise
+const user = createOrder(cart);
+
+// producer portion of promise
+function createOrder(cart) {
+    // creating the promise
+    const pr = new Promise(function (resolve, reject) {
+        // first validation for placing the order
+        // const validOrder = false;
+        if (!validOrder(cart)) {
+            const err = new Error("cart is not valid pam loves jim");
+            reject(err);
+        }
+
+        // logic for creating the order
+        // after we'll be going the orderId from the backend
+        const orderId = "orderId";
+        if (orderId) {
+            resolve(orderId);
+        }
+    });
+
+    // returning the promise
+    return pr
+}
+
+
+function validOrder(cart) {
+    return cart.length > 0;
+}
+
+user.then((data) => console.log(data));
