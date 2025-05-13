2a03be2b98e375cbed022131e34858eccfbb9a33
Oct - test <oct@email.com>
2025-05-14 00:41:59 +0530

    epi 3 only summary is remaining

--- howToCreateThePromise/src/script.js
+++ howToCreateThePromise/src/script.js
@@ -2,7 +2,16 @@
 const cart = [];
 
 // consuming the pormise
-const user = createOrder(cart);
+createOrder(cart)
+    .then((data) => { return data })
+    .catch(function (err) {
+        console.log(err.message)
+    }) // which make every then handler is independed from one another,
+    .then((data) => {
+        console.log("no matters, I'll be definitly be called")
+        return processedWithPayment("data");
+    })
+    .then((data) => { console.log(data) })
 
 // producer portion of promise
 function createOrder(cart) {
@@ -26,16 +35,20 @@ function createOrder(cart) {
     });
 
     // returning the promise
-    return pr
+    return pr;
 }
 
+function processedWithPayment(orderId) {
+    // logic for implementing the processed with payment thing
+    return new Promise(function (resolve, reject) {
+        if (orderId) {
+            resolve("processed with the payment");
+        } else {
+            reject(new Error("payment has failled to proceed"));
+        }
+    })
+}
 
 function validOrder(cart) {
     return cart.length > 0;
 }
-
-user
-    .then((data) => { console.log(data) })
-    .catch(function (err) {
-        console.log(err.message)
-    })
